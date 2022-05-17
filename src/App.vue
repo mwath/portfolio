e4ebb775901d920a6f947ca7e2e3f2981965b307
Mathieu Walravens <m.walravens@students.ephec.be>
2022-05-17 05:56:01 +0200

    reinstall vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,26 @@
 <template>
-  <v-app theme="dark">
-    <v-navigation-drawer app>
-      <v-list>
-        <v-list-item
-          prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
-          title="Mathieu Walravens"
-          subtitle="m.walravens@proton.me"
-        />
-      </v-list>
-      <v-divider></v-divider>
-      <v-list v-for="item in items" :key="item.title" nav density="compact">
-        <v-divider v-if="item.divider" />
-        <v-list-item
-          v-else
-          :title="item.title"
-          :prepend-icon="item.icon"
-          :to="{ hash: `#${item.url}` }"
-        />
-      </v-list>
-    </v-navigation-drawer>
+  <v-app>
     <v-main>
-      <router-view />
+      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
-<script setup lang="ts">
-const items = [
-  { title: "A propos de moi", icon: "mdi-face-shimmer", url: "about" },
-  { title: "Formation", icon: "mdi-school", url: "education" },
-  { title: "Expérience professionnelle", icon: "mdi-briefcase", url: "work" },
-  { title: "Langues", icon: "mdi-translate", url: "languages" },
-  { divider: true },
-  { title: "Activités", icon: "mdi-human-scooter", url: "activities" },
-  { title: "Résumé", icon: "mdi-table-of-contents", url: "summary" },
-];
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+
+  components: {
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      //
+    };
+  },
+});
 </script>
