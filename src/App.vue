633058b3f0498752d3ad66f7306d411f18f7d5c2
Mathieu Walravens <m.walravens@students.ephec.be>
2022-05-17 06:07:09 +0200

    Restore files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <v-app>
+  <v-app theme="dark">
+    <v-navigation-drawer app>
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
+          title="Mathieu Walravens"
+          subtitle="m.walravens@proton.me"
+        />
+      </v-list>
+      <v-divider></v-divider>
+      <v-list v-for="item in items" :key="item.title" nav density="compact">
+        <v-divider v-if="item.divider" />
+        <v-list-item
+          v-else
+          :title="item.title"
+          :prepend-icon="item.icon"
+          :to="{ hash: `#${item.url}` }"
+        />
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld />
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "App",
-
-  components: {
-    HelloWorld,
-  },
-
-  data() {
-    return {
-      //
-    };
-  },
-});
+<script setup lang="ts">
+const items = [
+  { title: "A propos de moi", icon: "mdi-face-shimmer", url: "about" },
+  { title: "Formation", icon: "mdi-school", url: "education" },
+  { title: "Expérience professionnelle", icon: "mdi-briefcase", url: "work" },
+  { title: "Langues", icon: "mdi-translate", url: "languages" },
+  { divider: true },
+  { title: "Activités", icon: "mdi-human-scooter", url: "activities" },
+  { title: "Résumé", icon: "mdi-table-of-contents", url: "summary" },
+];
 </script>
